186bd8cb73d21a290ceec92b73fc2637f46de34c
mari-j1407b <mariabelenmg1809@gmail.com>
2025-07-29 09:47:54 -0600

    Add files via upload

---
+++ LABORATORIO.py
@@ -0,0 +1,39 @@
+# LABORATORIO
+
+título = "LABORATORIO"
+print("-" * len(título) + "\n" + título + "\n" + "-" * len(título))
+
+# PARTE 1
+
+#1
+
+nombres = ["Ana", "Luis", "Pedro"]
+
+#2 Utilizamos .remove luego de escribir el nombre de la lista y luego entre paréntesis y comillas el nombre del elemento a  eliminar.
+
+nombres.remove("Ana")
+
+#3  Qué hace el siguiente código?
+
+while True:
+    print("Hola")
+    break
+# Este código "printea" Hola mientras el ciclo while indicado por el booleano sea verdadero
+
+#4
+
+persona = {
+    "nombre": "María",
+    "edad": 18,
+    "carrera": "Ingeniería en Sistemas"
+}
+print(persona)
+
+#5  La diferencia entre una tupla y una lista es 
+
+tupla = ("Ana", "Luis", "Pedro")
+print(tupla)
+
+lista = ["Ana", "Luis", "Pedro"]
+print(lista)
+
